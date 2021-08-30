a2c5fc894e5745a61055bd4f6cf5d8f0cfbedb04
Tauheed <tauheedahmad2003@gmail.com>
2021-08-30 20:40:44 +0530

    Initial Commit

---
+++ MVCDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCDemo
+//
+//  Created by Tauheed Ahmad on 30/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
