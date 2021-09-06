d78c75a7e7511f21a902a4a83ee22bbc7d773d92
Tauheed <tauheedahmad2003@gmail.com>
2021-09-06 09:48:21 +0530

    Dev2003: Design UI.

--- MVCDemo/ViewController.swift
+++ MVCDemo/ViewController.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var updateLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     // tauheeda@mindfire made some major changes.
-
+    @IBAction func updateLableAction(_ sender: Any) {
+    }
+    
 }
 
