58d3cccce0b4182e18950eeca601ab6052c2a7a2
tauheed-mindfire <tauheeda@mindfiresolutions.com>
2021-09-06 08:04:27 +0530

    // tauheeda@mindfire made some major changes.

--- MVCDemo/DevMindfireVC.swift
+++ MVCDemo/DevMindfireVC.swift
@@ -1,20 +1,21 @@
 //
-//  ViewController.swift
+//  DevMindfireVC.swift
 //  MVCDemo
 //
-//  Created by Tauheed Ahmad on 30/8/21.
+//  Created by Tauheed Ahmad on 6/9/21.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class DevMindfireVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
     // tauheeda@mindfire made some major changes.
 
 }
-
