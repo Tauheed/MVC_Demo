9a034bb4d83c1154ff398293cb8969bf0e7b9ecc
tauheed-mindfire <tauheeda@mindfiresolutions.com>
2021-09-07 09:54:41 +0530

    Dev-mindfire: Implemented business logic.

--- MVCDemo/ViewController.swift
+++ MVCDemo/ViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var updateLbl: UILabel!
+    var nameArry = [String]()
+    var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        nameArry = ["Tauheed", "Ahmad", "Ansari"]
+        updateLbl.text = nameArry[index]
     }
 
     // tauheeda@mindfire made some major changes.
     @IBAction func updateLableAction(_ sender: Any) {
+        
+        if (index < nameArry.count-1) {
+            index += 1
+        }
+        else {
+            index = 0
+        }
+        
+        updateLbl.text = nameArry[index]
     }
     
 }
