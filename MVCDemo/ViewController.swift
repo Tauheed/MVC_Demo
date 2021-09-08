956b44520cb64f89eae243e7630b79a19db0037f
tauheed-mindfire <tauheeda@mindfiresolutions.com>
2021-09-08 08:37:31 +0530

    Dev2003: Implemented moved all business login into MVC architecture.

--- MVCDemo/ViewController.swift
+++ MVCDemo/ViewController.swift
@@ -7,40 +7,25 @@
 
 import UIKit
 
-struct Employee {
-    let name: String
-}
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var updateLbl: UILabel!
-    var nameArry = [Employee]()
-    var index = 0
+    
+    let employeeObj = EmployeeModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        let emp1 = Employee(name: "Tauheed")
-        let emp2 = Employee(name: "Ahmad")
-        let emp3 = Employee(name: "Ansari")
-        nameArry = [emp1, emp2, emp3]
-        
-        updateLbl.text = nameArry[index].name
-        /////
+        updateLbl.text = employeeObj.nameArry[0].name
     }
 
     // tauheeda@mindfire made some major changes.
     @IBAction func updateLableAction(_ sender: Any) {
-        
-        if (index < nameArry.count-1) {
-            index += 1
-        }
-        else {
-            index = 0
-        }
-        
-        updateLbl.text = nameArry[index].name
+                
+        let empObj = employeeObj.updateEmployee()
+        updateLbl.text = empObj.name
     }
     
 }
